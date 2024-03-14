ec8651cb445296a784ab122a88727bef33131684
Evgeny Sharaev <ipsharaev@gmail.com>
2024-03-14 19:55:05 +0800

    Report csv is done

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ API_KEY = "8855876211e9e685efb3f3362691ac517d358f47406b2bcf988ab88ef3c40ca5"
 
 API_FILE_URL = "https://www.virustotal.com/api/v3/files"
 API_FILE_ANALYSES_URL = "https://www.virustotal.com/api/v3/analyses"
+FILE_PATH = "invoice-42369643.xlsm"
--- main.py
+++ main.py
@@ -4,88 +4,77 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import requests
-from config import API_KEY, API_FILE_URL, API_FILE_ANALYSES_URL
-
-
-file_path = "invoice-42369643.xlsm"
-headers = {"accept": "application/json", "x-apikey": API_KEY}
-payload = {"file": "/home/kar/Downloads/invoice-42369643.xlsm"}
-
-# Step 1. Unzip file
-
-# Step 2. Upload file on VirusTotal server
-with open(file_path, "rb") as file:
-    files = {"file": (file_path, file)}
-    response = requests.post(API_FILE_URL, headers=headers, files=files)
-    _id = response.json()["data"]["id"]
-
-
-
-
-
-
-
-# Step 3. Analyze file on malware
-
-url = "{}/{}".format(API_FILE_ANALYSES_URL, _id)
-response = requests.get(url, headers=headers)
-report = response.json()
-
-
-
-
-# creat spisok dangeres
-
-
-# data_danger = [list for list in report['data']['attributes']['results'].values()]
-# print(*data_danger, sep="\n")
-
-list_pd = report['data']['attributes']['results'] # для заполнения матрицы
-data_danger_pd= [[list['engine_name'], list['result']] for list in list_pd .values() if list['result']]
-x_data_danger_pd = sorted(list({x[0] for x in data_danger_pd}))
-y_data_danger_pd = sorted(list({y[1] for y in data_danger_pd}))
-# print(x_data_danger_pd, y_data_danger_pd)
-# print(*data_danger_pd, sep="\n")
-# print(data_danger_pd)
-
-matrix = [["-"]*len(x_data_danger_pd ) for _ in range(len(y_data_danger_pd ))]
-# print(*matrix, sep="\n")
-# print(data_danger_pd)
-
-for y in range(len(y_data_danger_pd)):
-    print(y_data_danger_pd[y])
-    for x in range(len(x_data_danger_pd)):
-        for antivirus, virus in data_danger_pd:
-
-            if x_data_danger_pd[x] == antivirus and y_data_danger_pd[y] == virus:
-
-                if "-" == matrix[y][x]:
-                    matrix[y][x] = 1
-                else:
-                    matrix[y][x] +=1
-
-# print(*matrix, sep="\n")
-
-
+import numpy as np
 import pandas as pd
-tadl = pd.DataFrame(matrix, index=y_data_danger_pd, columns = x_data_danger_pd)
-print(tadl)
+import json
+from config import API_KEY, API_FILE_URL, API_FILE_ANALYSES_URL, FILE_PATH
 
 
+def unzip_file(file_path):
+    """
+    Unzip file
+    :param file_path:
+    :return: file_path
+    """
+    pass
 
 
+def upload_file(filepath: str) -> int:
+    """
+    Upload file on VirusTotal server
+    :param filepath:
+    :return: id of the file
+    """
+    headers = {"accept": "application/json", "x-apikey": API_KEY}
+
+    with open(filepath, "rb") as file:
+        files = {"file": (filepath, file)}
+        response = requests.post(API_FILE_URL, headers=headers, files=files)
+        return response.json()["data"]["id"]
 
 
+def analyze_file(local_file_id: int) -> dict:
+    """
+    Analyze file on malware
+    :param local_file_id:
+    :return:
+    """
+    headers = {"accept": "application/json", "x-apikey": API_KEY}
+    url = "{}/{}".format(API_FILE_ANALYSES_URL, local_file_id)
+
+    response = requests.get(url, headers=headers)
+    return response.json()
 
 
+def create_dataframe(report_path: str):
+    """
+    Create pandas dataframe from json
+    :param report_path: str
+    :return: Pandas DF
+    df = [
+        [0, 1, 0, 1, 0, 0, 0, 1],
+        [1, 0, 1, 1, 0, 1, 0, 0],
+        [...]
+    ]
+    """
+    with open(report_path) as f:
+        report = json.load(f)["data"]["attributes"]["results"]
+    result = []
+    for software_name, report in report.items():
+        if_detected = None if report["result"] is None else "Detected"
+
+        result.append([
+            report["engine_name"],
+            if_detected,
+            report["result"]
+        ])
+    return pd.DataFrame(result, columns=["engine", "if_detected", "malware"])
 
 
+def create_report(dataframe):
+    dataframe.to_csv("data.csv", index=False, sep="\t")
+
+    return 0
 
 
-
-
-
-# url_behaviors = "{}/{}/behaviours".format(API_FILE_URL, _id)
-# print(url_behaviors)
-# # response = requests.get(url_behaviors, headers=headers)
-# # print(response.text)
+create_report(create_dataframe("data.json"))
